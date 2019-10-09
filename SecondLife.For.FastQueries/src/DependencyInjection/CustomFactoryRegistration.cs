5596d3fe06d4dc4f950026b56aa50b5d9856096c
Nikolay Mitikov <mitikov@users.noreply.github.com>
2019-10-09 10:32:59 +0300

    Plug in config to create custom databases

---
+++ SecondLife.For.FastQueries/src/DependencyInjection/CustomFactoryRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Abstractions;
+using Sitecore.DependencyInjection;
+
+namespace SecondLife.For.FastQueries.DependencyInjection
+{
+    public sealed class CustomFactoryRegistration: IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<BaseFactory, DefaultFactoryForCacheableFastQuery>();
+        }
+    }
+}
